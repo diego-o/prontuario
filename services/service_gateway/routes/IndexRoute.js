b35fd274591f72101dc27c92b9f7e63417de4621
diego-o <diego_1souza@hotmail.com>
2019-03-02 19:24:08 -0300

    Adicionado doc tipoOeganizacao

--- services/service_gateway/routes/IndexRoute.js
+++ services/service_gateway/routes/IndexRoute.js
@@ -1,6 +1,134 @@
 'use strict';
 
 exports.Add = function(app) {
+    /**
+     * @swagger
+     * components:
+     *   schemas:
+     *     Paciente:
+     *       properties:
+     *         nome:
+     *           type: string
+     *         cpf:
+     *           type: string
+     *         rg:
+     *           type: string
+     *         carteiraSus:
+     *           type: string
+     *         sexo:
+     *           type: string
+     *         dataNascimento:
+     *           type: string
+     *           format: date
+     *         dataObito:
+     *           type: string
+     *           format: date
+     *         telefone:
+     *           type: string
+     *         pai:
+     *           type: object
+     *           properties:
+     *             nome:
+     *               type: string
+     *             cpf:
+     *               type: string
+     *             telefone:
+     *               type: string
+     *         mae:
+     *           type: object
+     *           properties:
+     *             nome:
+     *               type: string
+     *             cpf:
+     *               type: string
+     *             telefone:
+     *               type: string
+     *         endereco:
+     *           type: object
+     *           properties:
+     *             nascimento:
+     *               type: object
+     *               properties:
+     *                 cep:
+     *                   type: string
+     *                 logradouro:
+     *                   type: string
+     *                 numero:
+     *                   type: string
+     *                 bairro:
+     *                   type: string
+     *                 municipio:
+     *                   type: string
+     *                 uf:
+     *                   type: string
+     *             atual:
+     *               type: object
+     *               properties:
+     *                 cep:
+     *                   type: string
+     *                 logradouro:
+     *                   type: string
+     *                 numero:
+     *                   type: string
+     *                 bairro:
+     *                   type: string
+     *                 municipio:
+     *                   type: string
+     *                 uf:
+     *                   type: string
+     * 
+     *     Medico:
+     *       properties:
+     *         crm:
+     *           type: object
+     *           properties:
+     *             numero:
+     *               type: string
+     *             uf: 
+     *               type: string
+     *         nome:
+     *           type: string
+     *         cpf:
+     *           type: string
+     *         endereco:
+     *           type: object
+     *           properties:
+     *             cep:
+     *               type: string
+     *             logradouro:
+     *               type: string
+     *             numero:
+     *               type: string
+     *             bairro:
+     *               type: string
+     *             municipio:
+     *               type: string
+     *             uf:
+     *               type: string
+     *         contato:
+     *           type: object
+     *           properties:
+     *             telefone:
+     *               type: string
+     *             email:
+     *               type: string
+     *         ativo:
+     *           type: boolean
+     * 
+     *     TipoOrganizacao:
+     *       properties:
+     *         codigo:
+     *           type: integer
+     *         tipo:
+     *           type: string
+     *      
+     *     ArrayOfTipoOrganizacao:
+     *       properties:
+     *         type: array
+     *         items:
+     *           $ref: '#/paths/components/schemas/TipoOrganizacao'
+    */
+
     //version Gateway
     app.route('/').get((req, res, next) => {
         res.status(200).send({
--- services/service_gateway/routes/OrganizacaoRoute.js
+++ services/service_gateway/routes/OrganizacaoRoute.js
@@ -17,10 +17,112 @@ exports.Add = function(app) {
     app.get('/organizacao/enfermeiro/cpf/:cpf', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
     app.post('/organizacao/enfermeiro/coren', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
 
+    /**
+     * @swagger
+     * /organizacao/medico:
+     *   post:
+     *     tags:
+     *       - Medico
+     *     description: Inserir novo Medico
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: medico
+     *         description: Objeto Medico
+     *         in: body
+     *         required: true
+     *         schema:
+     *           $ref: '#/paths/components/schemas/Medico'
+     *     responses:
+     *       200:
+     *         description: Successfully created
+     */
     app.post('/organizacao/medico', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
+
+    /**
+     * @swagger
+     * /organizacao/medico/{id}:
+     *   put:
+     *     tags: 
+     *       - Medico
+     *     description: Atualizar um Medico
+     *     produces: 
+     *       - application/json
+     *     parameters:
+     *       - name: id
+     *         in: path
+     *         required: true
+     *         type: string
+     *       - name: medico
+     *         description: Objeto Medico
+     *         in: body
+     *         required: true
+     *         schema:
+     *           $ref: '#/paths/components/schemas/Medico'
+     *     responses:
+     *       200:
+     *         description: Successfully updated
+     */
     app.put('/organizacao/medico/:id', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
+
+    /**
+     * @swagger
+     * /organizacao/medico/{id}:
+     *   get:
+     *     tags:
+     *       - Medico
+     *     description: Consultar Medico pelo id
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: id
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Objeto Medico
+     */
     app.get('/organizacao/medico/:id', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
+
+    /**
+     * @swagger
+     * /organizacao/medico/crm/{crm}:
+     *   get:
+     *     tags:
+     *       - Medico
+     *     description: Consultar Medico pelo crm
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: crm
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Objeto Medico
+     */
     app.post('/organizacao/medico/crm', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
+
+    /**
+     * @swagger
+     * /organizacao/medico/cpf/{cpf}:
+     *   get:
+     *     tags:
+     *       - Medico
+     *     description: Consultar Medico pelo cpf
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: cpf
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Objeto Medico
+     */
     app.get('/organizacao/medico/cpf/:cpf', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
 
     app.post('/organizacao/organizacao', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
@@ -29,6 +131,42 @@ exports.Add = function(app) {
     app.get('/organizacao/organizacao/:id', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
     app.get('/organizacao/organizacao/cnpj/:cnpj', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
 
+    /**
+     * @swagger
+     * /organizacao/tipoorganizacao:
+     *   post:
+     *     tags:
+     *       - TipoOrganizacao
+     *     description: Inserir novo Tipo de Organização
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: tipoOrganizacao
+     *         description: Objeto TipoOrganizacao
+     *         in: body
+     *         required: true
+     *         schema:
+     *           $ref: '#/paths/components/schemas/TipoOrganizacao'
+     *     responses:
+     *       200:
+     *         description: Successfully created
+     */
     app.post('/organizacao/tipoorganizacao', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
+
+    /**
+     * @swagger
+     * /organizacao/tipoorganizacao:
+     *   get:
+     *     tags:
+     *       - TipoOrganizacao
+     *     description: Lista todos os tipos de organização
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: Array de TipoOrganizacao
+     *         schema:
+     *           $ref: '#/paths/components/schemas/TipoOrganizacao'
+     */
     app.get('/organizacao/tipoorganizacao', (req, res, next) => { ServiceCadastroOrganizacao(req, res, next) });
 }
