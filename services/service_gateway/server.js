75e64669193b6723fddf3705d996880704f7e23c
diego-o <diego_1souza@hotmail.com>
2019-03-02 17:08:06 -0300

    Adicionando o Swagger

--- services/service_gateway/server.js
+++ services/service_gateway/server.js
@@ -4,6 +4,31 @@ var http = require('http');
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
+var swaggerJSDoc = require('swagger-jsdoc');
+var path = require('path');
+
+var swaggerDefinition = {
+    info: {
+       title: 'Documentação API Prontuário Eletrônico',
+       version: '1.0.0.0',
+       description: '',
+    },
+    host: 'localhost:' + port,
+    basePath: '/'
+};
+var options = {
+    swaggerDefinition: swaggerDefinition,
+    apis: ['./routes/*.js']
+};
+var swaggerSpec = swaggerJSDoc(options);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/swagger.json', function (req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   res.send(swaggerSpec);
+});
 
 var IndexRoute = require('./routes/IndexRoute');
 IndexRoute.Add(app);
--- services/service_paciente/controller/PacienteController.js
+++ services/service_paciente/controller/PacienteController.js
@@ -26,7 +26,6 @@ exports.Update = async(req, res, next) => {
 exports.GetById = async(req, res, next) => {
     try {
         var paciente = await pacienteDao.GetById(req.params.id);
-        console.log("consulta de paciente executada");
         res.status(200).send(paciente);
     } catch (error) {
         res.status(500).send({ Message: 'Erro ao consultar paciente.', Data: error });
