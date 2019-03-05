4b9d514ef488579e1ddea25531347352506991f7
diego-o <diego_1souza@hotmail.com>
2019-03-05 15:27:31 -0300

    .

--- appPaciente/src/app/services/service-base/base.service.ts
+++ appPaciente/src/app/services/service-base/base.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class BaseService {
+  
   private _urlBase = "http://35.198.41.47/";
 
   constructor(private http: HttpClient) { }
