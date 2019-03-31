80ed2e27a5d2d80a6c1790b6abe31f2b3962b3b3
diego-o <diego_1souza@hotmail.com>
2019-03-31 13:53:22 -0300

    Adicionado Toast

--- appPaciente/src/app/login/login.page.ts
+++ appPaciente/src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/models/usuario-model';
 import { PacienteService } from 'src/services/service-paciente/paciente.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,13 @@ import { PacienteService } from 'src/services/service-paciente/paciente.service'
 export class LoginPage implements OnInit {
   usuario: Usuario;
   message: string = "";
-  
+
   constructor(
     private router: Router,
     private LoginService: LoginService,
-    private PacienteService: PacienteService
-  ) { 
+    private PacienteService: PacienteService,
+    private Toast: ToastController
+  ) {
     this.usuario = new Usuario();
   }
 
@@ -28,23 +30,35 @@ export class LoginPage implements OnInit {
     return this.usuario;
   }
 
-  Logar(){
+  Logar() {
     this.message = "";
     this.LoginService.Logar(this.usuario).subscribe(user => {
-      if (user != null){
+      if (user != null) {
         this.PacienteService.getPaciente(this.usuario.cpf).subscribe(paciente => {
           this.PacienteService.SetPaciente(paciente);
 
           this.router.navigate(['tabs']);
           this.usuario = new Usuario();
-        });        
+        });
       } else {
-        this.message = "Falha na autenticação"
+        this.message = "Falha na autenticação";
+        this.ShowToast(this.message);
       }
     }, error => {
-      this.message = "Erro ao tentar se autenticar."
+      this.message = "Erro ao tentar se autenticar.";
+      this.ShowToast(error.message);
       console.log(error);
     });
+  }
+
+  async ShowToast(Message: string) {
+    const toast = await this.Toast.create({
+      message: Message,
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'OK'
+    });
+    toast.present();
   }
 
 }
